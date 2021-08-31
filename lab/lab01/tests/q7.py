3a78d8bd6c63253fa20b31353f049eef13bf921f
Rohan Jha <rohan.jha@berkeley.edu>
2021-08-30 22:29:16 -0700

    added student facing version of lab 1

---
+++ lab/lab01/tests/q7.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q7',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> curve_1 in ["elastic", "inelastic"]\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> curve_2 in ["elastic", "inelastic"]\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
