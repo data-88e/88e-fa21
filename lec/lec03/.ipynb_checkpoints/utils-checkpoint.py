2b556d032e701ecfd6e3cd160442cb2d6c4c4f96
Rohan Jha <rohan.jha@berkeley.edu>
2021-09-14 12:57:42 -0700

    updated lec03 notebook

--- lec/lec03/.ipynb_checkpoints/utils-checkpoint.py
+++ lec/lec03/.ipynb_checkpoints/utils-checkpoint.py
@@ -11,7 +11,7 @@ def firm_behaviour(price, individual_firm_costs):
     
     output = individual_firm_costs.column("Output")[1:]
     mc = individual_firm_costs.column("Marginal Cost")[1:]
-    sp_mc = csaps(output, mc, smooth=0.85)
+    sp_mc = csaps.csaps(output, mc, smooth=0.85)
     output_s = np.linspace(output.min(), output.max(), 150)
     mc_s = sp_mc(output_s)
 
