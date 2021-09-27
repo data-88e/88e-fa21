48a4b270d9e55f08c93c38bbb3a8a0baf50d1f20
Rohan Jha <rohan.jha@berkeley.edu>
2021-09-27 12:14:12 -0700

    updated lab04

--- lab/lab04/tests/q1_1.py
+++ lab/lab04/tests/q1_1.py
@@ -1,5 +1,5 @@
 test = {   'name': 'q1_1',
-    'points': 1,
+    'points': None,
     'suites': [   {   'cases': [   {'code': '>>> np.isclose(cobb_douglas(1, 2, 2, 0.5), 2.0000000000000004)\nTrue', 'hidden': False, 'locked': False},
                                    {'code': '>>> np.isclose(cobb_douglas(1, 4, 0.5, 0.2, 1.1), 0.6155722066724582)\nTrue', 'hidden': False, 'locked': False},
                                    {'code': '>>> cobb_douglas(1, 2, 2, 0.5) == cobb_douglas(1, 2, 2, 0.5, 0.5)\nTrue', 'hidden': False, 'locked': False}],
--- lab/lab04/tests/q2_5.py
+++ lab/lab04/tests/q2_5.py
@@ -1,5 +1,5 @@
 test = {   'name': 'q2_5',
-    'points': 1,
+    'points': None,
     'suites': [   {   'cases': [{'code': '>>> np.all(countries == data.group("country").column("country"))\nTrue', 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
