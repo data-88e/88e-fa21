822457c9d57a04a53e29f266faf6c5a4617894b7
Rohan Jha <rohan.jha@berkeley.edu>
2021-10-12 00:20:16 -0700

    updated lab 6

---
+++ lab/lab06/tests/q2_1.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q2_1',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> len(cum_income_share)\n5', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.sum(np.diff(cum_income_share) >= 0) == 4\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab/lab06/tests/q2_7.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q2_7',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> type(k_hat) in [float, np.float64, np.float32]\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
