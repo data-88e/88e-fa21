fbe7e86a1585497530c47a5a232b62c4fb625851
Ergun Acikoz <ergunackz@gmail.com>
2021-11-11 14:52:45 -0800

    updated lab10

---
+++ lab/lab10/tests/q2_2.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q2_2',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> len(X_q2_dummies.labels) == 20\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ lab/lab10/tests/q3_2.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q3_2',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> (q3_2_y == rebates.column("dcs")).all()\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> 0 <= model_q3_2.params.taxreb <= 0.5\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
